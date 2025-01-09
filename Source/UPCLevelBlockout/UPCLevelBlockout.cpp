7ceb01de7b2c643cb45654fd54b28a21e3a6674e
lganter <lukasganter@gmail.com>
2025-01-09 13:10:54 +0100

    Initial commit

---
+++ Source/UPCLevelBlockout/UPCLevelBlockout.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UPCLevelBlockout.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UPCLevelBlockout, "UPCLevelBlockout" );
+
+DEFINE_LOG_CATEGORY(LogUPCLevelBlockout)
+ 
